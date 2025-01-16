3f82ba23d9160d089c3b16869c60ea2dc898d4b6
Mateus-msj3 <mateussouzaads@outlook.com>
2025-01-16 11:31:30 -0300

    first commit

---
+++ src/main/java/io/github/msj/swiftbank/SwiftBankApplication.java
@@ -0,0 +1,13 @@
+package io.github.msj.swiftbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftBankApplication.class, args);
+	}
+
+}
